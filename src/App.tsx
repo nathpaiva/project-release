f11527538e82fe4955a8852ab2e97b5dd32cb441
Nath Paiva <hello@nathpaiva.com.br>
2021-05-18 11:52:06 -0300

    feat: adds new value

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Test da nath
+          Add new change
         </a>
       </header>
     </div>
