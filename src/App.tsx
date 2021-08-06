77e7449a1e7163eed8a69e5ecbc6c0f0bb846e5e
Nath Paiva <hello@nathpaiva.com.br>
2021-08-06 11:24:14 -0300

    feat: changes text

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Add new change Hello new
+          Remove and test
         </a>
       </header>
     </div>
